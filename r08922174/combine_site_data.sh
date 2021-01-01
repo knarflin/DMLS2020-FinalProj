1119abc1268c0ffb2c7ce81bbafaccd62abb3137
Huangyuren <forautodesk1@gmail.com>
2021-01-02 03:02:58 +0800

    [Add] utility script help warping up image folders for centralized trainig

---
+++ r08922174/combine_site_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+path_prefix="./splitdata/train/"
+mkdir ${path_prefix}total
+cp -r ${path_prefix}site0/* ${path_prefix}total/
+
+for counter in $(seq 1 3)
+do
+    for site_counter in $(seq -f "%05g" 0 42)
+    do
+        DIR="${path_prefix}site${counter}/${site_counter}"
+        for f in "$DIR"/*
+        do
+            cp $f ${path_prefix}total/${site_counter}/
+        done
+    done
+done
